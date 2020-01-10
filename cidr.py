0436825d68cc96e7078029018130cafd89f9ec9d
Shani <shujaajunk@gmail.com>
2020-01-10 21:21:22 +0000

    Testing

--- cidr.py
+++ cidr.py
@@ -2,11 +2,33 @@
 
 import sublime, sublime_plugin
 
-class ExampleCommand(sublime_plugin.TextCommand):
+class CidrOnCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 
-		myRegionSet = self.view.find_all("255\.255\.255\.0")
-		for region in myRegionSet:
-			self.view.replace(edit, region, "/24")
+		ipmasks = self.view.find_all("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} 255\.255\.255\.255")
+		
+		
+		for region in ipmasks:
+			regionstring = self.view.substr(region)
+			#self.view.replace(edit, region, "/32")
 
-		self.view.insert(edit, 0, "Done")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		self.view.insert(edit, 0, regionstring)
