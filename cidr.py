2a7f4c959159db909fc32d84ad5e0133a758f9a6
Shani <shujaajunk@gmail.com>
2020-01-11 08:23:44 +0000

    Prototype

--- cidr.py
+++ cidr.py
@@ -6,29 +6,29 @@ class CidrOnCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 
 		ipmasks = self.view.find_all("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} 255\.255\.255\.255")
-		
-		
+		regionstring = ""
+		masks = []
+
 		for region in ipmasks:
 			regionstring = self.view.substr(region)
-			#self.view.replace(edit, region, "/32")
+			regionstring = regionstring.split()
+			ip = regionstring[0]
+			mask = regionstring[1]
+			iplen = len(ip)
+			masklen = len(mask)
+			regionend = region.end()
+			regionstart = regionend - masklen - 1
+			region = sublime.Region(regionstart, regionend)
+			newregion = self.view.substr(region)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		self.view.insert(edit, 0, regionstring)
+			self.view.replace(edit, region, "/32")
+			#debug:
+			self.view.insert(edit, self.view.size(),
+				"\nDEBUG:"
+				 + "\nIP: " + ip
+				 + "\nMask: " + mask
+				 + "\niplen: " + str(iplen)
+				 + "\nmasklen: " + str(masklen)
+				 + "\nregionend: " + str(regionend)
+				 + "\nregionstart: " + str(regionstart)
+				 + "\nnewregion: " + newregion)
